a3bd34e9c59c5ac67092cf83e3ebe009a2ee7a1a
deergod <CommonTroll@live.co.uk>
2023-04-24 16:08:23 +0100

    working on email validation

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -41,9 +41,7 @@ const checkPass = async (req, res, next) => {
 
 const checkEmail = async (req, res, next) => {
   try {
-    if (String(req.body.email)
-        .toLowerCase()
-        .match(
+    if (String(req.body.email).toLowerCase().match(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       )) {
         console.log("valid email");
