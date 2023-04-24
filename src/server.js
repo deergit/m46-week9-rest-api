d9a014d29e1d3dc994f32f7d7d4f771a089c9881
deergod <CommonTroll@live.co.uk>
2023-04-24 11:25:09 +0100

    updating server.js to use userRouter and the User model for syncing the table

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const User = require("./users/model");
+const userRouter = require("./users/routes");
 
 const port = process.env.PORT || 5001;
 
@@ -8,7 +10,10 @@ app.use(express.json());
 
 const syncTables = () => {
   // for later syncing of tables
+  // User.sync();
 }
+
+app.use(userRouter);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "App running correctly" });
